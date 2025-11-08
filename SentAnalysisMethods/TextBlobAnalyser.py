51a9f3da1a27953fe2cf750c6139be302c3170fb
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-11-08 11:24:07 +0000

    docs: adding files that will be needed and further struct

--- SentAnalysisMethods/TextBlobAnalyser.py
+++
@@ -1,6 +0,0 @@
-"""
-Created on Mon Aug 18 23:29:41 2025
-
-@author: matthew
-"""
-
--- SentAnalysisMethods/VaderAnalyser.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 18 23:29:14 2025
-
-@author: matthew
-"""
-
