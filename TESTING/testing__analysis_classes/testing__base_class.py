32934632af77c84d435f2a2c5b78e4afdf398297
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-12-13 20:59:18 +0000

    test: validate results tests PASSING

--- TESTING/testing__analysis_classes/testing__base_class.py
+++ TESTING/testing__analysis_classes/testing__base_class.py
@@ -1,13 +1,13 @@
 import unittest
 from sa_models.base_class import SentimentAnalyserBase
+from sa_models.sentiment_result import SentimentResult
+
 
 class testing__base_class(unittest.TestCase):
 
-    """
-    Testing the static score_to_label function, expecting to classify the scores
-    passed to it. Function has customisable thresholds for classification, and should
-    react accordingly.
-    """
+    # # # # # # # # # #
+    # score_to_label  #
+    # # # # # # # # # #
     def test_score_to_label_positive(self):
         result = SentimentAnalyserBase.score_to_label(score=0.6)
         self.assertEqual(result, "positive")
@@ -25,3 +25,46 @@ class testing__base_class(unittest.TestCase):
             0.1, pos_threshold=0.2
         )
         self.assertEqual(result, "neutral")
+
+    # # # # # # # # # #
+    # validate_result #
+    # # # # # # # # # #
+
+    def test_validate_result(self):
+        result = SentimentResult(
+            text="hello",
+            score=0.3,
+            label="positive"
+        )
+
+        SentimentAnalyserBase.validate_result(result)
+
+    def test_validate_result_invalid_label(self):
+        result = SentimentResult(
+            text="hello",
+            score=0.3,
+            label="britain"
+        )
+
+        with self.assertRaises(ValueError):
+            SentimentAnalyserBase.validate_result(result)
+
+    def test_validate_result_text(self):
+        result = SentimentResult(
+            text=42,
+            score=0.3,
+            label="positive"
+        )
+
+        with self.assertRaises(TypeError):
+            SentimentAnalyserBase.validate_result(result)
+
+    def test_validate_result_score(self):
+        result = SentimentResult(
+            text="hello",
+            score="banana",
+            label="positive"
+        )
+
+        with self.assertRaises(TypeError):
+            SentimentAnalyserBase.validate_result(result)
