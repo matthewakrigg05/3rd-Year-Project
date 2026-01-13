85101eb923f49888f49ad62979d72665c1536bf9
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-01-13 15:43:13 +0000

    fix: gitignoring more files unintentionally

---
+++ EXPLORE_REQUIREMENTS/api_conn.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Load .env from project root
+env_path = Path(__file__).parent.parent / '.env'
+load_dotenv(dotenv_path=str(env_path))
+
+url = "https://api.twitterapi.io/twitter/tweet/advanced_search?query=politics"
+
+headers = {
+    "X-API-Key": f"{os.getenv("API_KEY")}"
+}
+
+# response = requests.get(url, headers=headers)
+
+# print(response.text)
+
+print(headers)
