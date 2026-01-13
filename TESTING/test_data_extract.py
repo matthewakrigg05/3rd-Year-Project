cdcddbc5d081b81036c2d56ccc7466fd6dc2f8c3
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-01-13 16:24:28 +0000

    tests: moved function

--- TESTING/test_data_extract.py
+++ TESTING/test_data_extract.py
@@ -1,22 +1,5 @@
 import unittest
-import re
-
-def extract_english_text(api_response):
-    texts = []
-
-    for tweet in api_response.get("tweets", []):
-        if tweet.get("lang") != "en":
-            continue
-
-        text = tweet.get("text", "")
-        text = re.sub(r"@\w+", "", text)  # remove mentions
-        text = re.sub(r"\s+", " ", text).strip()
-
-        if text:
-            texts.append(text)
-
-    return texts
-
+from src.data_collection.data_handling import extract_english_text
 
 class TestExtractEnglishText(unittest.TestCase):
     def test_extracts_only_english_text(self):
