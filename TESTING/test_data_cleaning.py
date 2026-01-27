d3007ce638789ae09b55b6723f89b81f25a9408b
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-01-27 15:41:21 +0000

    test: added a url test

--- TESTING/test_data_cleaning.py
+++ TESTING/test_data_cleaning.py
@@ -1,3 +1,9 @@
 import unittest
 
 class TestDataClean(unittest.TestCase):
+    def test_remove_urls(self):
+        text = ["Check this link: https://example.com and http://test.com"]
+        self.assertEqual(
+            remove_urls(text),
+            ["Check this link:  and "]
+        )
