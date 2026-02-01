d35c484ddca994b3a399c064a05604c54a09a9b0
Matthew Akrigg <matthewakrigg05@gmail.com>
2026-02-01 13:25:03 +0000

    test: Tests for splitting up hashtags, keeping their sentiments

--- TESTING/test_data_cleaning.py
+++ TESTING/test_data_cleaning.py
@@ -1,6 +1,26 @@
 import unittest
 
-class TestDataClean(unittest.TestCase):
+class TestSplitCamelHashtags(unittest.TestCase):
+    def test_splits_camel_case(self):
+        self.assertEqual(split_camel_hashtag("BestDayEver"), "Best Day Ever")
+
+    def test_handles_acronym_boundary(self):
+        # "JSONData" should become "JSON Data"
+        self.assertEqual(split_camel_hashtag("JSONData"), "JSON Data")
+
+    def test_all_upper_kept_as_one_token(self):
+        self.assertEqual(split_camel_hashtag("LOL"), "LOL")
+
+    def test_all_lower_kept_as_one_token(self):
+        self.assertEqual(split_camel_hashtag("worstdayever"), "worstdayever")
+
+    def test_single_word_unchanged(self):
+        self.assertEqual(split_camel_hashtag("Hello"), "Hello")
+
+    
+
+
+class TestPreprocessTweet(unittest.TestCase):
     def test_remove_urls(self):
         text = ["Check this link: https://example.com and http://test.com"]
         self.assertEqual(
